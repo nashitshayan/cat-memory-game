0f07c25330d792071eb9fe453248313535ac7660
Nashit Shayan <shayan.nashit@gmail.com>
2022-07-02 14:39:42 +0530

    Generate Cats Array

---
+++ src/catData.js
@@ -0,0 +1,33 @@
+import images from './importImages';
+const catData = [];
+const names = [
+	'Mudpie',
+	'Midnight',
+	'Ashy',
+	'Sansa',
+	'Brownie',
+	'Moochie',
+	'Cookie',
+	'Poochie',
+	'Tobby',
+	'Greg',
+	'Elisha',
+	'Honey',
+	'Fred',
+	'Floof',
+	'Sparkle',
+	'Sushi',
+];
+
+for (let i = 0; i < 16; i++) {
+	let id = catData.length + 1;
+	let name = `cat-${id}.png`;
+	let url = images[name];
+	catData.push({
+		id: id,
+		url: url,
+		name: names[i],
+	});
+}
+
+export { catData };
